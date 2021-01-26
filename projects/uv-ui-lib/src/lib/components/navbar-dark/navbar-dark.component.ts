f40a52b3428f1193fd2174a22572b9d8f1132c8e
amandasimonds <amandasimonds9@gmail.com>
2021-01-26 12:33:32 -0700

    added image source and link inputs for navbar component

--- projects/uv-ui-lib/src/lib/components/navbar-dark/navbar-dark.component.ts
+++ projects/uv-ui-lib/src/lib/components/navbar-dark/navbar-dark.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'lib-navbar-dark',
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar-dark.component.scss']
 })
 export class NavbarDarkComponent implements OnInit {
+
+  @Input() icon1: string;
+  @Input() icon2: string;
+  @Input() icon3: string;
+  @Input() userPic: string;
+  @Input() navLink1: string;
+  @Input() navLink2: string;
+  @Input() navLink3: string;
+  @Input() userLink: string;
 
   constructor() { }
 
